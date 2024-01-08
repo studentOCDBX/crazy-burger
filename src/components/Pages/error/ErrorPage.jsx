f313ee78cdc6bd770eeaf785df004954c41f6709
Nazaire DARBOUX <studentdbx@gmail.com>
2024-01-08 14:14:07 +0100

    ✨ F02: Implement routing system

---
+++ src/components/Pages/error/ErrorPage.jsx
@@ -0,0 +1,6 @@
+function ErrorPage() {
+    return (
+        <div>ErrorPage</div>
+    )
+}
+export default ErrorPage
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LoginPage from './components/pages/login/LoginPage'
+import ErrorPage from './components/pages/error/ErrorPage.jsx'
+import OrderPage from './components/pages/order/OrderPage.jsx'
 
+const router = createBrowserRouter([
+  {
+    errorElement: < ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <LoginPage />
+      },
+      {
+        path: "/order",
+        element: <OrderPage />
+      },
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
