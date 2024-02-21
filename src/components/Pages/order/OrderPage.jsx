05a2fe20e0444cfd972af4cf758950d2aa05bdbc
Nazaire DARBOUX <studentdbx@gmail.com>
2024-02-21 18:30:58 +0100

    🚧F04:OrderPage> Setting up the page layout

--- src/components/Pages/order/OrderPage.jsx
+++ src/components/Pages/order/OrderPage.jsx
@@ -1,4 +1,6 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import theme from '../../../theme/index.js'
 
 function OrderPage() {
     // state (état, données)
@@ -6,15 +8,56 @@ function OrderPage() {
 
     // Comportements
 
-    //  Affichage(render) 
+    //  Affichage(render)
     return (
-        <>
-            <h1>Bonjour {username}</h1>
-            <br />
-            <Link to="/">
-                <button>Déconnexion</button>
-            </Link>
-        </>
+        <OrderPageStyled>
+            <div className="container">
+                <div className="navbar">
+                    navbar
+                    <h1>Bonjour {username}</h1>
+                    <br />
+                    <Link to="/">
+                        <button>Déconnexion</button>
+                    </Link>
+                </div>
+                <div className="main">Main</div>
+            </div>
+        </OrderPageStyled>
     )
 }
 export default OrderPage
+const OrderPageStyled = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: ${theme.colors.primary};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    .container {
+        border: 1px solid red;
+        display: flex;
+        flex-direction: column;
+        width: 92%;
+        max-width: 1400px;
+        height: 95vh;
+        position: absolute;
+        z-index: 1;
+        border-radius: 15px;
+
+        .main {
+            width: 100%;
+            flex: 1;
+            background-color: #fff;
+            border-radius: 0px 0px 15px 15px;
+            box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+        }
+    }
+    .navbar {
+        /* border: 1px solid black; */
+        background-color: green;
+        height: 11%;
+        border-radius: 15px 15px 0 0;
+    }
+`
