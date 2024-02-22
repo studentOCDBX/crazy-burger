ef1d821c5531e56839062f1fa8f7997b0ff8fa0e
Nazaire DARBOUX <studentdbx@gmail.com>
2024-02-22 13:42:10 +0100

    ✨F04:OrderPage>Adding <Navbar/> component right part in OrderPage.

---
+++ src/components/Pages/order/Navbar.jsx
@@ -0,0 +1,90 @@
+import { Link, useParams } from 'react-router-dom'
+import styled from 'styled-components'
+import Logo from '../../reusableUi/Logo.jsx'
+import { BsPersonCircle } from 'react-icons/bs'
+import theme from '../../../theme/index.js'
+
+function NavBar() {
+    // state (état, données)
+    const { username } = useParams()
+
+    // Comportements
+
+    //  Affichage(render)
+    return (
+        <NavBarStyled>
+            <Logo className="burger__logo" />
+            <div className="navbar__wrapper">
+                <div className="navbar__wrapper__body">
+                    <p>
+                        Hey,
+                        <span>{username}</span>
+                    </p>
+                    <Link className="btn__link" to="/">
+                        Se déconnecter
+                    </Link>
+                </div>
+                <BsPersonCircle className="navbar__icon" />
+            </div>
+        </NavBarStyled>
+    )
+}
+
+export default NavBar
+
+const NavBarStyled = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #fff;
+    height: 11%;
+    border-radius: 15px 15px 0 0;
+    &.burger__logo {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        width: 240px;
+        transform: none;
+    }
+    .navbar__wrapper {
+        height: 44px;
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        margin-right: 70px;
+        .navbar__wrapper__body {
+            display: flex;
+            flex: 1;
+            gap: 0.2rem;
+            flex-direction: column;
+            align-items: center;
+            font-family: Open Sans;
+            font-weight: 400;
+            & p {
+                display: flex;
+                justify-content: center;
+                font-size: 16px;
+                line-height: 22px;
+                gap: 0.5rem;
+                margin: 0;
+                & span {
+                    font-weight: 700;
+                    line-height: 22px;
+                    color: ${theme.colors.primary};
+                }
+            }
+            & .btn__link {
+                width: 100%;
+                line-height: 14px;
+                letter-spacing: 0em;
+                text-align: right;
+            }
+        }
+        .navbar__icon {
+            width: 36px;
+            height: 36px;
+            color: ${theme.colors.greyDark};
+        }
+    }
+`
--- src/components/Pages/order/OrderPage.jsx
+++ src/components/Pages/order/OrderPage.jsx
@@ -1,10 +1,9 @@
-import { Link, useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import theme from '../../../theme/index.js'
+import NavBar from '../../reusableUi/NavBar.jsx'
 
 function OrderPage() {
     // state (état, données)
-    const { username } = useParams()
 
     // Comportements
 
@@ -12,14 +11,7 @@ function OrderPage() {
     return (
         <OrderPageStyled>
             <div className="container">
-                <div className="navbar">
-                    navbar
-                    <h1>Bonjour {username}</h1>
-                    <br />
-                    <Link to="/">
-                        <button>Déconnexion</button>
-                    </Link>
-                </div>
+                <NavBar />
                 <div className="main">Main</div>
             </div>
         </OrderPageStyled>
@@ -37,15 +29,14 @@ const OrderPageStyled = styled.div`
     position: relative;
     .container {
         border: 1px solid red;
+        position: absolute;
+        z-index: 1;
         display: flex;
         flex-direction: column;
         width: 92%;
         max-width: 1400px;
         height: 95vh;
-        position: absolute;
-        z-index: 1;
         border-radius: 15px;
-
         .main {
             width: 100%;
             flex: 1;
@@ -53,11 +44,5 @@ const OrderPageStyled = styled.div`
             border-radius: 0px 0px 15px 15px;
             box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
         }
-    }
-    .navbar {
-        /* border: 1px solid black; */
-        background-color: green;
-        height: 11%;
-        border-radius: 15px 15px 0 0;
     }
 `
