aaa88876a3173c5932e2cce358fa8f763d2a287e
Nazaire DARBOUX <studentdbx@gmail.com>
2024-02-25 17:55:23 +0100

    ♻️ F04:OrderPage>Navbar>refactor code create <Profile/> Component.

--- src/components/Pages/order/Navbar.jsx
+++ src/components/Pages/order/Navbar.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Logo from '../../reusableUi/Logo.jsx'
 import { BsPersonCircle } from 'react-icons/bs'
 import theme from '../../../theme/index.js'
+import Profile from './Profile.jsx'
 
 function NavBar() {
     // state (état, données)
@@ -18,18 +19,7 @@ function NavBar() {
             </Link>
             <div className="navbar__rightPart">
                 {/* <div className="admin__button">Admin Button</div> */}
-                <div className="user__profile">
-                    <div className="user__profile__body">
-                        <p>
-                            Hey,
-                            <span>{username}</span>
-                        </p>
-                        <Link className="btn__link" to="/">
-                            Se déconnecter
-                        </Link>
-                    </div>
-                    <BsPersonCircle className="user__picture" />
-                </div>
+                <Profile />
             </div>
         </NavBarStyled>
     )
@@ -53,48 +43,7 @@ const NavBarStyled = styled.div`
         padding: 32px 0;
         height: auto;
     }
-    .user__profile {
-        height: 44px;
-        display: flex;
-        align-items: center;
-        gap: 1rem;
-        .user__profile__body {
-            display: flex;
-            flex: 1;
-            gap: 0.2rem;
-            flex-direction: column;
-            align-items: center;
-            font-weight: 400;
-            & p {
-                display: flex;
-                justify-content: center;
-                font-size: 16px;
-                line-height: 22px;
-                gap: 0.5rem;
-                margin: 0;
-                & span {
-                    font-weight: 700;
-                    line-height: 22px;
-                    color: ${theme.colors.primary};
-                }
-            }
-            & .btn__link {
-                width: 100%;
-                line-height: 14px;
-                text-align: right;
-                &:hover {
-                    padding-bottom: 8px;
-                    border-bottom: 2px solid ${theme.colors.greyMedium};
-                }
-            }
-        }
-        .user__picture {
-            width: 36px;
-            height: 36px;
-            color: ${theme.colors.greyDark};
-        }
-    }
-    .navbar__rightPart {
+    /* .navbar__rightPart {
         display: flex;
         justify-content: center;
         align-items: center;
@@ -102,5 +51,5 @@ const NavBarStyled = styled.div`
             background: pink;
             border: 2px solid purple;
         }
-    }
+    } */
 `
