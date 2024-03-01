faffb12d031cbd6a5ba25769090d00fec9ce8737
Nazaire DARBOUX <studentdbx@gmail.com>
2024-03-01 06:50:10 +0100

    🚧F05:OrderPage>Main>Add Grid layout.

--- src/components/Pages/order/Main.jsx
+++ src/components/Pages/order/Main.jsx
@@ -1,8 +1,20 @@
 import styled from 'styled-components'
 import theme from '../../../theme/index.js'
+import { useState } from 'react'
+import fakeMenu2 from '../../../fakeData/fakeMenu.js'
 
 function Main() {
-    return <MainStyled className="main"></MainStyled>
+    // state (état, données)
+    const [menus, setMenus] = useState(fakeMenu2)
+    // Comportements
+
+    //  Affichage(render)
+    return (
+        <MainStyled>
+            <div className="orderbasket">basket</div>
+            <div className="menuBoard">Menu</div>
+        </MainStyled>
+    )
 }
 export default Main
 
@@ -12,4 +24,12 @@ const MainStyled = styled.div`
     background-color: ${theme.colors.background_white};
     border-radius: 0px 0px 15px 15px;
     box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+    display: grid;
+    grid-template-columns: 25% 1fr;
+    .orderbasket {
+        background: #ee08ad;
+    }
+    .menuBoard {
+        background: cyan;
+    }
 `
